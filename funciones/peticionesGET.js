9f8f860e74afa941b56f8ea11306d1007faaed74
Nelson Blanco Charro <nbcharro@gmail.com>
2023-03-30 17:53:41 +0200

    peticiones GET

--- funciones/peticionesGET.js
+++ funciones/peticionesGET.js
@@ -1,37 +1,60 @@
 function obtenerTodosDatos() {
-    const datos = require('../datos.json');
-    return datos;
+	const datos = require('../alimentos.json');
+	return datos;
 }
 
-function listaProductos() {
-    const datosJSON = require('../datos.json');
-    let productos = [];
-    datosJSON.forEach(element => {
-        productos.push(element.title);
-    });
-    console.log(productos);
-    return productos;
+function listaAlimentos() {
+	const datosJSON = require('../alimentos.json');
+	let productos = [];
+	datosJSON.forEach(element => {
+		productos.push(element.Alimento);
+	});
+	return productos;
 }
 
 function obtenerDatosPorID(id) {
-    const datosJSON = require('../datos.json');
-    let datos = false;
-    datosJSON.forEach(element => {
-        if (element.id == id) {
-            datos = element;
-        }
-    });
-    return datos;
-}
-function obtenerMayor(caracteristica) {
-    const datosJSON = require('../datos.json');
-    let datos = datosJSON[0];
-    datosJSON.forEach(element => {
-        if (eval(`element.${caracteristica}`) > eval(`datos.${caracteristica}`)) {
-            datos = element;
-        }
-    });
-    return datos;
+	const datosJSON = require('../alimentos.json');
+	let datos = false;
+	datosJSON.forEach(element => {
+		if (element.id == id) {
+			datos = element;
+		}
+	});
+	return datos;
 }
 
-module.exports = { obtenerTodosDatos, listaProductos, obtenerDatosPorID, obtenerMayor };
+function obtenerDatosPorNombre(nombreAlimento) {
+	const datosJSON = require('../alimentos.json');
+	let datos = false;
+	console.log(nombreAlimento);
+	datosJSON.forEach(element => {
+		if (element.Alimento == nombreAlimento) {
+			datos = element;
+		}
+	});
+	return datos;
+}
+
+function obtenerMayor(caracteristica) {
+	const datosJSON = require('../alimentos.json');
+	let datos = datosJSON[0];
+	datosJSON.forEach(element => {
+		if (eval(`element.${caracteristica}`) > eval(`datos.${caracteristica}`)) {
+			datos = element;
+		}
+	});
+	return datos;
+}
+
+function obtenerMenor(caracteristica) {
+	const datosJSON = require('../alimentos.json');
+	let datos = datosJSON[0];
+	datosJSON.forEach(element => {
+		if (eval(`element.${caracteristica}`) < eval(`datos.${caracteristica}`)) {
+			datos = element;
+		}
+	});
+	return datos;
+}
+
+module.exports = { obtenerTodosDatos, listaAlimentos, obtenerDatosPorID, obtenerDatosPorNombre, obtenerMayor, obtenerMenor };
--- index.js
+++ index.js
@@ -8,59 +8,80 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 /* app.get('/hola/:name/:id',(req,res)=>{
-    res.status(200).send({message: `Hello ${req.params.name} con id: ${req.params.id}!`});
+	res.status(200).send({message: `Hello ${req.params.name} con id: ${req.params.id}!`});
 }) */
 
 /* GET */
-app.get('/api/v1/products', (req, res) => {
-    const funcion = require('./funciones/peticionesGET');
-    const datos = funcion.obtenerTodosDatos();
-    res.status(200).send(datos);
+app.get('/api/v1/alimentos', (req, res) => {
+	const funcion = require('./funciones/peticionesGET');
+	const datos = funcion.obtenerTodosDatos();
+	res.status(200).send(datos);
 })
 
-app.get('/api/v1/products/lista', (req, res) => {
-    const funcion = require('./funciones/peticionesGET');
-    const productos = funcion.listaProductos();
-    res.status(200).send(productos);
+app.get('/api/v1/alimentos/lista', (req, res) => {
+	const funcion = require('./funciones/peticionesGET');
+	const alimentos = funcion.listaAlimentos();
+	res.status(200).send(alimentos);
 })
 
-app.get('/api/v1/products/:productId', (req, res) => {
-    const funcion = require('./funciones/peticionesGET');
-    const datos = funcion.obtenerDatosPorID(req.params.productId);
-    if (datos) {
-        res.status(200).send(datos);
-    } else {
-        res.status(404).send({ message: "No encontrado" });
-    }
+// No funciona, ARREGLAR
+// app.get('/api/v1/alimentos/:productId', (req, res) => {
+// 	const funcion = require('./funciones/peticionesGET');
+// 	const datos = funcion.obtenerDatosPorID(req.params.productId);
+// 	if (datos) {
+// 		res.status(200).send(datos);
+// 	} else {
+// 		res.status(404).send({ message: "No encontrado" });
+// 	}
+// })
+
+app.get('/api/v1/alimentos/:nombre', (req, res) => {
+	const funcion = require('./funciones/peticionesGET');
+	const datos = funcion.obtenerDatosPorNombre(req.params.nombre.toLocaleUpperCase());
+	if (datos) {
+		res.status(200).send(datos);
+	} else {
+		res.status(404).send({ message: "No encontrado" });
+	}
 })
 
-app.get('/api/v1/products/mayor/:caracteristica', (req, res) => {
-    const funcion = require('./funciones/peticionesGET');
-    const datos = funcion.obtenerMayor(req.params.caracteristica);
-    if (datos) {
-        res.status(200).send(datos);
-    } else {
-        res.status(404).send({ message: "No encontrado" });
-    }
+app.get('/api/v1/alimentos/mayor/:caracteristica', (req, res) => {
+	const funcion = require('./funciones/peticionesGET');
+	const datos = funcion.obtenerMayor(req.params.caracteristica);
+	if (datos) {
+		res.status(200).send(datos);
+	} else {
+		res.status(404).send({ message: "No encontrado" });
+	}
+})
+
+app.get('/api/v1/alimentos/menor/:caracteristica', (req, res) => {
+	const funcion = require('./funciones/peticionesGET');
+	const datos = funcion.obtenerMenor(req.params.caracteristica);
+	if (datos) {
+		res.status(200).send(datos);
+	} else {
+		res.status(404).send({ message: "No encontrado" });
+	}
 })
 
 /* POST */
-app.post('/api/v1/products', (req, res) => {
-    console.log(req.body);
-    res.status(200).send({ message: 'El producto se ha recibido' });
+app.post('/api/v1/alimentos', (req, res) => {
+	console.log(req.body);
+	res.status(200).send({ message: 'El producto se ha recibido' });
 })
 
 /* PUT */
-app.put('api/v1/product/:productId', (req, res) => {
+app.put('api/v1/alimentos/:productId', (req, res) => {
 
 })
 
 /* DELETE */
-app.delete('api/v1/product/:productId', (req, res) => {
+app.delete('api/v1/alimentos/:productId', (req, res) => {
 
 })
 
 /*  */
 app.listen(port, () => {
-    console.log(`API REST corriendo en http://localhost:${port}`);
+	console.log(`API REST corriendo en http://localhost:${port}`);
 })
