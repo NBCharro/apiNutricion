6e0594f2855e31b5ef084307b4894cf7e3a77f05
Nelson Blanco Charro <nbcharro@gmail.com>
2023-04-23 16:44:35 +0200

    agregada pagina principal de informacion

--- build/src/index.js
+++ build/src/index.js
@@ -4,12 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const alimentos_1 = __importDefault(require("./routes/alimentos"));
+const renderpug_1 = __importDefault(require("./routes/renderpug"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const cors = require('cors');
 app.use(cors());
 const PORT = process.env.PORT || 3000;
+// Configuración de Pug para mostrar la home
+app.set('views', path_1.default.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use('/', renderpug_1.default);
+// Funcionalidad de la API
 app.use('/api/v1/', alimentos_1.default);
 app.listen(PORT, () => {
     console.log(`Server is running in port ${PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express from 'express'
-import diaryRouter from './routes/alimentos'
+import express from 'express';
+import path from 'path';
+import alimentosRouter from './routes/alimentos'
+import renderpug from './routes/renderpug'
 
 const app = express()
 app.use(express.json())
@@ -7,7 +9,13 @@ const cors = require('cors');
 app.use(cors());
 const PORT = process.env.PORT || 3000
 
-app.use('/api/v1/', diaryRouter)
+// Configuración de Pug para mostrar la home
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use('/', renderpug);
+
+// Funcionalidad de la API
+app.use('/api/v1/', alimentosRouter)
 
 app.listen(PORT, () => {
 	console.log(`Server is running in port ${PORT}`);
