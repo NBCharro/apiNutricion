c15234da44d454e6d20511e3b03c6d36de20bd8c
Nelson Blanco Charro <nbcharro@gmail.com>
2023-05-06 19:29:15 +0200

    api simplificada

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import path from 'path';
 import alimentosRouter from './routes/alimentos'
-import renderpug from './routes/renderpug'
 
 const app = express()
 app.use(express.json())
@@ -9,10 +7,6 @@ const cors = require('cors');
 app.use(cors());
 const PORT = process.env.PORT || 3000
 
-// Configuración de Pug para mostrar la home
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use('/', renderpug);
 
 // Funcionalidad de la API
 app.use('/api/v1/', alimentosRouter)
@@ -20,3 +14,4 @@ app.use('/api/v1/', alimentosRouter)
 app.listen(PORT, () => {
 	console.log(`Server is running in port ${PORT}`);
 })
+
